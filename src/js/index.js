00c8abd1b663918c00b11cba2f7093eb29d19ad3
Emilse Arias <emilsearias@gmail.com>
2020-07-20 18:22:20 -0300

    se agrego la logica de la pagina principal

--- src/js/index.js
+++ src/js/index.js
@@ -2,5 +2,32 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap';
 import '../css/style.css';
 
-let comision = "comision 3A"
-console.log("desde el archivo index.js "+ comision);
+leerProductos();
+
+function leerProductos() {
+	if (localStorage.length > 0) {
+		let listaProductos = JSON.parse(localStorage.getItem('funkopopKey'));
+
+		//borrar las cards
+
+		let catalogo = document.getElementById('catalogo');
+
+		//crear las cards
+		let codHTML = '';
+		for (let i in listaProductos) {
+			codHTML = `<div class="col-sm-12 col-md-4 col-lg-3">
+            <div class="card">
+              <img src="img/productos/${listaProductos[i].imagen}" class="card-img-top" alt="funkopop${listaProductos[i]
+				.nombre}">
+              <div class="card-body">
+                <h5 class="card-title">${listaProductos[i].nombre}</h5>
+                <p class="card-text">${listaProductos[i].descripcion}</p>
+                <a href="#" class="btn btn-secondary disabled">ver mas</a>
+              </div>
+            </div>
+          </div>`;
+
+			catalogo.innerHTML += codHTML;
+		}
+	}
+}
